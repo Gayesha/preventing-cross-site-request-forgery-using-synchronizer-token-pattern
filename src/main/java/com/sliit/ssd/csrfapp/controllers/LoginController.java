f6d7f3310e2a6119f291eee201824b714fdb9746
Gayesha Suraweera <gayeshaachini@gmail.com>
2018-10-26 20:47:23 +0530

    Initial commit

---
+++ src/main/java/com/sliit/ssd/csrfapp/controllers/LoginController.java
@@ -0,0 +1,45 @@
+package com.sliit.ssd.csrfapp.controllers;
+
+import com.sliit.ssd.csrfapp.models.Credentials;
+import com.sliit.ssd.csrfapp.services.AuthenticationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Controller for handling login requests
+ *
+ * Created by dinukshakandasamanage on 9/5/18.
+ */
+
+@Controller
+public class LoginController {
+
+    private Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @Autowired
+    AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute Credentials credentials, HttpServletResponse response){
+        String username = credentials.getUsername();
+
+        if(authenticationService.isUserAuthenticated(username, credentials.getPassword())){
+
+            logger.debug("Successfully authenticated user...");
+            Cookie sessionCookie = new Cookie("sessionID", authenticationService.generateSessionId(username));
+            Cookie userCookie = new Cookie("username", username);
+            response.addCookie(sessionCookie);
+            response.addCookie(userCookie);
+            return "redirect:/home";
+        }
+        logger.debug("Failed to authenticate user...");
+        return "redirect:/login?status=failed";
+    }
+}
